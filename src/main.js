2d09d520e0bded9b6acc99e29a4027fa82d893b8
soomin <sooper1137@gmail.com>
2023-12-20 01:31:36 +0900

    기술 스택 추가

--- src/main.js
+++ src/main.js
@@ -6,10 +6,8 @@ import header from '@scripts/header.js';
 import cursor from '@scripts/cursor.js';
 import canvas from '@scripts/canvas.js';
 import interactive from '@scripts/interactive.js';
-import matter from '@scripts/matter.js';
 
 header();
 cursor();
 canvas();
 interactive();
-matter();
